e59b7857d23161153e9a7aa6e043f9a01dc0d4d2
Gruvix <timmy.burton@hotmail.com>
2023-09-16 11:10:56 -0300

    Comente tarea 1
    comente tarea 1 para poder hacerla de 0

--- tarea1.js
+++ tarea1.js
@@ -1,12 +1,23 @@
-document.querySelector('#calcular-salario-mensual').onclick = function(){
-    const salarioAnual = Number(document.querySelector('#salario-anual').value);
-    const salarioMensual = calcularSalarioMensual(salarioAnual);
 
-    document.querySelector('#salario-mensual').value = salarioMensual;
 
-    return false;
-}
 
-function calcularSalarioMensual(salarioAnual){
-    return salarioAnual / 12;
-}
+
+
+
+
+
+
+
+
+// document.querySelector('#calcular-salario-mensual').onclick = function(){
+//     const salarioAnual = Number(document.querySelector('#salario-anual').value);
+//     const salarioMensual = calcularSalarioMensual(salarioAnual);
+
+//     document.querySelector('#salario-mensual').value = salarioMensual;
+
+//     return false;
+// }
+
+// function calcularSalarioMensual(salarioAnual){
+//     return salarioAnual / 12;
+// }
