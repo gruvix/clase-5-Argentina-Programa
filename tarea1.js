44e3c9735598470ae75f479ad35ab586b92015dc
Gruvix <timmy.burton@hotmail.com>
2023-09-16 11:29:25 -0300

    Add calcular salario mensual
    calcula salario mensual a aprtir de anual y actualiza valores en html

--- tarea1.js
+++ tarea1.js
@@ -1,5 +1,21 @@
 
 
+const $botonCalcularSalarioMensual = document.querySelector("#calcular-salario-mensual")
+$botonCalcularSalarioMensual.addEventListener("click", calcularSalarioMensual) 
+
+function calcularSalarioMensual(){
+    const salarioAnual = Number(document.querySelector('#salario-anual').value);
+    let salarioMensual = salarioAnual / 12;
+    document.querySelector('#salario-mensual').value = salarioMensual;
+}
+
+
+
+
+
+//TAREA: crear un formulario donde un usuario pueda ingresar su salario anual.
+//cuando el usuario haga click en el botón "calcular", mostrar el salario mensual
+// en una caja de texto deshabilitada. --> <input type="text" disabled id="salario-mensual"/>
 
 
 
