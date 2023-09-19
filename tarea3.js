4ba8bdf44d0326ce9704918b7a1712e4d7b2dc52
Gruvix <timmy.burton@hotmail.com>
2023-09-18 21:27:29 -0300

    Calculadora de tiempo
    Calculadora de tiempo, con ordenador (convierte 60 en 1 del siguiente
    imprime en consola

--- tarea3.js
+++ tarea3.js
@@ -1,5 +1,40 @@
+document.querySelector("#calcular").addEventListener("click", calcularTiempo)
+
+//( tiempo[0] = segundos tiempo[1] = minutos tiempo[2] = horas )
+let tiempo = [0, 0, 0]
+
+function calcularTiempo(){
+  tiempo = sumarContadorTiempo(tiempo)
+  tiempo = ordenarTiempo(tiempo)
+  imprimirTiempo()
+}
+
+function sumarContadorTiempo(){
+tiempoSuma = tiempo
+tiempoSuma[0] += Number(document.querySelector("#segundos").value)
+tiempoSuma[1] += Number(document.querySelector("#minutos").value)
+tiempoSuma[2] += Number(document.querySelector("#horas").value)
+return tiempoSuma
+}
+
+//ordena segundos, minutos y horas para que todo se convierta en la unidad mayor posible siendo 
+function ordenarTiempo(){
+let tiempoOrden = tiempo
+tiempoOrden[1] += Math.floor(tiempoOrden[0]/60)
+tiempoOrden[2] += Math.floor(tiempoOrden[1]/60)
+tiempoOrden[1] = tiempoOrden[1] % 60
+tiempoOrden[0] = tiempoOrden[0] % 60
+return tiempoOrden;
+}
+
+function imprimirTiempo() {
+  console.log(`${tiempo[2]}hs  ${tiempo[1]}'  ${tiempo[0]}"`)
+}
 
 
+tiempo.forEach(element => {
+  console.log(element)
+});
 
 //agregar funcion con boton de volver a menu de seleccion
 const $botonVolverASelector = document.querySelector("#volver-a-selector")
